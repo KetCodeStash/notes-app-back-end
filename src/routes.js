2380746caf4c5e8c7074362b8a9c5baff2d160bf
KetCodeStash <ketoffacc@gmail.com>
2024-12-20 16:18:15 +0700

    initial commit

---
+++ src/routes.js
@@ -0,0 +1,37 @@
+const {
+  addNoteHandle,
+  getAllNotesHandler,
+  getNoteByIdHandler,
+  editNoteByIdHandler,
+  deleteNoteByIdHandler
+} = require('./handler.js');
+
+const routes = [
+  {
+    method: 'POST',
+    path: '/notes',
+    handler: addNoteHandle
+  },
+  {
+    method: 'GET',
+    path: '/notes',
+    handler: getAllNotesHandler
+  },
+  {
+    method: 'GET',
+    path: '/notes/{id}',
+    handler: getNoteByIdHandler
+  },
+  {
+    method: 'PUT',
+    path: '/notes/{id}',
+    handler: editNoteByIdHandler,
+  },
+  {
+    method: 'DELETE',
+    path: '/notes/{id}',
+    handler: deleteNoteByIdHandler
+  }
+];
+
+module.exports = routes;
